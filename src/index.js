b0e0cd33c39cc63323d3c01a16b1522217d4ebd5
getamitgupta <amitguptacse2028@gmail.com>
2025-07-02 11:44:37 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import app from "./app.js";
 import db_connection from "./db/index.js";
 import dotenv from 'dotenv'
 dotenv.config();
 
-db_connection()
+db_connection().then(
+app.listen(process.env.PORT || 5000,()=>{
+    console.log(`server is ruinning on http://localhost:${process.env.PORT}`)
+})
+
+).catch((err)=>{
+    console.log("bhai error aggya",err);
+    
+})
